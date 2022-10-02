75b09767a62c510e59d87fd31ca2c5f659a3300b
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-10-02 09:23:02 +0530

    Volume Control

---
+++ Volume Control/sol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int x,y;
+    // vector<int> ans; ;
+	for(int i=0 ;i<n;i++){
+	   cin>>x>>y;
+	   cout<<y-x;
+	}
+  
+	return 0;
+}
