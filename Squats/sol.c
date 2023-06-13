554a0abd7dd0e898692a99cf996bc27067046634
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2023-06-13 18:21:28 +0530

    Create sol.c

---
+++ Squats/sol.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d", &n);
+	for(int i=0; i<n; i++){
+	    int x ;
+	    scanf("%d", &x);
+	    printf("%d\n",x *15);
+	    
+	}
+	return 0;
+}
+
